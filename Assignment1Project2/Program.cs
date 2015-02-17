e0f1652c2b5ddd379db3a0a2ffc494231ecee637
abenbrahim94 <abenbrahim94@gmail.com>
2015-02-17 10:53:24 -0500

    FinalCommit

---
+++ Assignment1Project2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            bool prompt;
+
+            //Codes
+            Console.WriteLine("\"Welcome to Program2\"");
+            Console.WriteLine();
+            Console.Write("Answer True or False ");
+            Console.WriteLine();
+            Console.WriteLine("Are you a male");
+            prompt = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Your Response is: \"{0}\"", prompt);
+            Console.WriteLine();
+            Console.WriteLine("\"Press any key to exit\"");
+            Console.ReadKey();
+        }
+    }
+}
